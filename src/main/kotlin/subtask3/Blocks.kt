aa84de73ad455bce2885e2b5dc655347660052c7
Bondidos <gglbond@gmail.com>
2021-06-11 01:34:28 +0300

    commit: finally!

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -5,34 +5,38 @@ import java.time.format.DateTimeFormatter
 import java.util.*
 import kotlin.reflect.KClass
 
-class Blocks {
+
+class Blocks  {
 
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
+        val str: KClass<*> = String::class
+        val int: KClass<*> = Int::class
+        val date: KClass<*> = LocalDate::class
 
-        when ( blockB.simpleName ?: "null"){
-            "String"    -> {
-                var result =""
-                blockA.filterIsInstance<String>().forEach{ result += it }
-                return result
-            }
-            "Int"       -> {
-                var result = 0
-                blockA.filterIsInstance<Int>().forEach{ result += it }
-                return result
-            }
-            "LocalDate" -> {
-                var result : LocalDate? =null
-
-                blockA.filterIsInstance<LocalDate>().forEach {
-
-                    if (result==null && it.isBefore(LocalDate.now()))
-                        result = it
-                    else if (result!!.isBefore(it) && it.isBefore(LocalDate.now())) result = it
-                }
-                val form = DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale("ru"))
-                return result?.format(form)!!
-            }
-            else        -> return "null"
+        if (blockB.java === str.java) {
+            var result = ""
+            blockA.filterIsInstance<String>().forEach { result += it }
+            return result
         }
+
+        if (blockB.java === int.java) {
+            var result = 0
+            blockA.filterIsInstance<Int>().forEach { result += it }
+            return result
+        }
+
+        if (blockB.java === date.java) {
+            var result: LocalDate? = null
+
+            blockA.filterIsInstance<LocalDate>().forEach {
+
+                if (result == null && it.isBefore(LocalDate.now()))
+                    result = it
+                else if (result!!.isBefore(it) && it.isBefore(LocalDate.now())) result = it
+            }
+            val form = DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale("ru"))
+            return result?.format(form)!!
+        }
+        return ""
     }
 }
