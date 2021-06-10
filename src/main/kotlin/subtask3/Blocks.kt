bde18a7e6a21f11703459fae24b5ef043ca31ae6
Bondidos <gglbond@gmail.com>
2021-06-11 00:11:45 +0300

    commit: 1 & 2 task 10 passed. trying task 3

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,38 @@
 package subtask3
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 import kotlin.reflect.KClass
 
 class Blocks {
 
-    // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+
+        when ( blockB.simpleName ?: "null"){
+            "String"    -> {
+                var result =""
+                blockA.filterIsInstance<String>().forEach{ result += it }
+                return result
+            }
+            "Int"       -> {
+                var result = 0
+                blockA.filterIsInstance<Int>().forEach{ result += it }
+                return result
+            }
+            "LocalDate" -> {
+                var result : LocalDate? =null
+
+                blockA.filterIsInstance<LocalDate>().forEach {
+
+                    if (result==null && it.isBefore(LocalDate.now()))
+                        result = it
+                    else if (result!!.isBefore(it) && it.isBefore(LocalDate.now())) result = it
+                }
+                val form = DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale("ru"))
+                return result?.format(form)!!
+            }
+            else        -> return "null"
+        }
     }
 }
