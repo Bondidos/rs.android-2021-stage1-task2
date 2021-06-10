0355ef185d04a2da57b5eadfe447764456f5a8cf
Bondidos <gglbond@gmail.com>
2021-06-11 00:00:35 +0300

    commit: trying autotest by 1 task

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,24 @@
 package subtask1
 
+import java.lang.IllegalArgumentException
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeParseException
+import java.util.*
+
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        val dateFormat = SimpleDateFormat("d MMMM, EEEE", Locale("ru"))
+        val date = GregorianCalendar ()
+        date.isLenient = false         // False
+        date.set(year.toInt(), month.toInt() - 1, day.toInt())
+        return try {
+            date.time
+            dateFormat.format(date.time) // true = Valid data entered for a date.
+        } catch (e: IllegalArgumentException) {
+            "Такого дня не существует" // Exception caught, meaning invalid data-entry.
+        }
     }
 }
